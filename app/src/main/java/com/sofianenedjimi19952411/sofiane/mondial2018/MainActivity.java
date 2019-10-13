0c6c27a7329de9bd7bb38d5e6bf1af17341508ab
sofiane <sofiane19952411@gmail.com>
2019-10-13 15:58:30 +0100

    World Cup 2018 summary

---
+++ app/src/main/java/com/sofianenedjimi19952411/sofiane/mondial2018/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sofianenedjimi19952411.sofiane.mondial2018;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+    }
+}
